9abb367e4d1eb669dc2f9d1bfa8e4fe487950c20
Emik <emik.lakunishok@gmail.com>
2022-11-28 17:24:27 +0100

    Change volume to byte

--- wawa.DDL/Source/Preferences.cs
+++ wawa.DDL/Source/Preferences.cs
@@ -9,7 +9,7 @@ public static class Preferences
 {
     /// <summary>The maximum volume allowed by the game and default value for the respective properties.</summary>
     [PublicAPI]
-    public const int MaxVolume = 100;
+    public const byte MaxVolume = 100;
 
     /// <summary>Gets a value indicating whether the game is being played on VR.</summary>
     /// <remarks><para>In the editor, this value always returns <see langword="false"/>.</para></remarks>
@@ -22,18 +22,19 @@ public static class Preferences
     /// <summary>Gets or sets the current music volume from the dossier menu. Ranges 0 to 100.</summary>
     /// <remarks><para>In the editor, this returns <see cref="MaxVolume"/>.</para></remarks>
     [PublicAPI]
-    public static int Music
+    public static byte Music
     {
-        [Pure] get => FromGame(0, static _ => PlayerSettingsManager.Instance.PlayerSettings.MusicVolume, MaxVolume);
+        [Pure]
+        get => FromGame(0, static _ => (byte)PlayerSettingsManager.Instance.PlayerSettings.MusicVolume, MaxVolume);
         set => FromGame(value, static v => PlayerSettingsManager.Instance.PlayerSettings.MusicVolume = v);
     }
 
     /// <summary>Gets or sets the current sound effect volume from the dossier menu. Ranges 0 to 100.</summary>
     /// <remarks><para>In the editor, this returns <see cref="MaxVolume"/>.</para></remarks>
     [PublicAPI]
-    public static int Sound
+    public static byte Sound
     {
-        [Pure] get => FromGame(0, static _ => PlayerSettingsManager.Instance.PlayerSettings.SFXVolume, MaxVolume);
+        [Pure] get => FromGame(0, static _ => (byte)PlayerSettingsManager.Instance.PlayerSettings.SFXVolume, MaxVolume);
         set => FromGame(value, static v => PlayerSettingsManager.Instance.PlayerSettings.SFXVolume = v);
     }
 }
