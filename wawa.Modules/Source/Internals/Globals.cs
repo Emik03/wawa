22e0dac47171e9287e6fd5484662b7dcd8800826
Emik <emik.lakunishok@gmail.com>
2022-11-28 21:12:53 +0100

    Remove redundant imports

--- wawa.Modules/Source/Internals/Globals.cs
+++ wawa.Modules/Source/Internals/Globals.cs
@@ -1,7 +1,6 @@
 // <copyright file="Globals.cs" company="Emik">
 // Copyright (c) Emik. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
-global using System.Collections.ObjectModel;
 global using Wawa.DDL;
 global using Wawa.Optionals;
 global using Wawa.Unity;
