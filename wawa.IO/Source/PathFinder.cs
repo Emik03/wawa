0bfa202a1ba5f54ad5c895c9b2bfb8a9556b6c75
Emik <emik.lakunishok@gmail.com>
2022-12-31 23:17:32 +0100

    Make version logging more efficient

--- wawa.IO/Source/PathFinder.cs
+++ wawa.IO/Source/PathFinder.cs
@@ -45,11 +45,11 @@ public static class PathFinder
     /// <summary>Gets the version of <see cref="Caller"/>.</summary>
     [NotNull]
     static string Which =>
-        Caller.Version is { Minor: 0, Build: 0, Revision: 0 } ver ? $"{ver.Major}" : $"{Caller.Version}";
+        Caller.Version is var ver && ver is { Minor: 0, Build: 0, Revision: 0 } ? $"v{ver.Major}" : $"v{ver}";
 
     /// <summary>Logs a message to the Unity Console with the assembly's name that called this.</summary>
     /// <param name="message">The message to log.</param>
-    public static void AssemblyLog([NotNull] string message) => Debug.Log($"[{Who} (v{Which})] {message}");
+    public static void AssemblyLog([NotNull] string message) => Debug.Log($"[{Who} ({Which})] {message}");
 
     /// <summary>Gets the absolute dir of the directory from folder mod.</summary>
     /// <param name="assembly">
