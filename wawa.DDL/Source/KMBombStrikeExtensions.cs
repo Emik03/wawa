4b43ddbbfff366fa6f8485701479959958eea3b1
Emik <emik.lakunishok@gmail.com>
2023-07-30 02:38:04 +0200

    Add parameter to conditionally sign flip

--- wawa.DDL/Source/KMBombStrikeExtensions.cs
+++ wawa.DDL/Source/KMBombStrikeExtensions.cs
@@ -7,7 +7,7 @@ using static Math;
 [PublicAPI]
 public static class KMBombStrikeExtensions
 {
-    // ReSharper disable Unity.PerformanceCriticalCodeInvocation
+    // ReSharper disable NullableWarningSuppressionIsUsed Unity.PerformanceCriticalCodeInvocation
 
     /// <summary>Gets the number of strikes.</summary>
     /// <remarks><para>In the editor, this value always returns 0.</para></remarks>
@@ -20,29 +20,25 @@ public static class KMBombStrikeExtensions
     /// <summary>Gets the rate in which the timer ticks.</summary>
     /// <remarks><para>In the editor, this value always returns 1.</para></remarks>
     /// <param name="that">The <see cref="KMBomb"/> module instance to grab the rate from.</param>
+    /// <param name="signFlip">
+    /// When true, invert the returned value when the component from <paramref name="that"/> ticks backwards.
+    /// </param>
     /// <returns>The speed of the ticking.</returns>
     [CLSCompliant(false), PublicAPI]
-    public static float GetRate([NotNull] this KMBomb that) =>
-        FromGame(that, static x => x.GetComponent<Bomb>().GetTimer().GetRate(), 1);
+    public static float GetRate([NotNull] this KMBomb that, bool signFlip = false) =>
+        FromGame(new Dangerous(that, 0, signFlip), static x => x.GetRate(), 1);
 
     /// <summary>Sets the rate in which the timer ticks.</summary>
     /// <remarks><para>In the editor, this method does nothing.</para></remarks>
     /// <param name="that">The <see cref="KMBomb"/> module instance to grab the rate from.</param>
     /// <param name="value">The value to set it to.</param>
+    /// <param name="signFlip">
+    /// When true, invert <paramref name="value"/> when the component from <paramref name="that"/> ticks backwards.
+    /// </param>
     /// <returns>The parameter <see cref="KMBomb"/>.</returns>
     [CLSCompliant(false), NotNull, PublicAPI]
-    public static KMBomb SetRate([NotNull] this KMBomb that, float value)
-    {
-        static byte Hidden(Mutation<float> x)
-        {
-            var (that, value) = x;
-            that.GetComponent<Bomb>().GetTimer().SetRateModifier(value);
-            return 0;
-        }
-
-        FromGame(new Mutation<float>(that, value), static x => Hidden(x));
-        return that;
-    }
+    public static KMBomb SetRate([NotNull] this KMBomb that, float value, bool signFlip = false) =>
+        FromGame(new Dangerous(that, value, signFlip), static x => x.SetRate(), that)!;
 
     /// <summary>Sets the number of strikes.</summary>
     /// <remarks><para>In the editor, this method does nothing.</para></remarks>
@@ -50,49 +46,41 @@ public static class KMBombStrikeExtensions
     /// <param name="value">The value to set it to.</param>
     /// <returns>The parameter <see cref="KMBomb"/>.</returns>
     [CLSCompliant(false), NonNegativeValue, NotNull, PublicAPI]
-    public static KMBomb SetStrikes([NotNull] this KMBomb that, int value)
+    public static KMBomb SetStrikes([NotNull] this KMBomb that, int value) =>
+        FromGame(new Dangerous(that, value), static x => x.SetStrikes(), that)!;
+
+    [StructLayout(LayoutKind.Auto)]
+    readonly struct Dangerous([NotNull] KMBomb bomb, int value, bool signFlip = false)
     {
-        static byte Hidden(Mutation<int> x)
+        internal unsafe Dangerous(KMBomb bomb, float value, bool signFlip)
+            : this(bomb, *(int*)&value, signFlip) { }
+
+        internal float GetRate()
         {
-            var (that, value) = x;
-            var inner = that.GetComponent<Bomb>();
+            var rate = bomb.GetComponent<Bomb>().GetTimer().GetRate();
+            return rate < 0 && signFlip ? -rate : rate;
+        }
+
+        internal KMBomb SetStrikes()
+        {
+            var inner = bomb.GetComponent<Bomb>();
             inner.StrikeIndicator.StrikeCount = inner.NumStrikes = value;
 
             // Reimplementation for tick rate found in [Assembly-CSharp]Bomb.OnStrike(MonoBehaviour)
             // The sign is set to match the current rate.
-            var sign = Sign(that.GetRate());
+            var sign = Sign(inner.GetTimer().GetRate());
 
             // Maps 0..4 to 1..2, rounded to the nearest quarter.
-            var clip = (value / 4f).Clip(0, 1) + 1;
+            var clip = Mathf.Clamp(inner.NumStrikes / 4, 0, 1) + 1;
 
-            that.SetRate(clip * sign);
-            return 0;
+            bomb.SetRate(clip * sign);
+            return bomb;
         }
 
-        FromGame(new Mutation<int>(that, value), static x => Hidden(x));
-        return that;
-    }
-
-    static float Clip(this float value, float min, float max) =>
-        value <= min ? min :
-        value >= max ? max : value;
-
-    sealed class Mutation<T>
-    {
-        readonly KMBomb _bomb;
-
-        readonly T _value;
-
-        internal Mutation(KMBomb bomb, T value)
+        internal unsafe KMBomb SetRate()
         {
-            _bomb = bomb;
-            _value = value;
-        }
-
-        internal void Deconstruct(out KMBomb bomb, out T value)
-        {
-            bomb = _bomb;
-            value = _value;
+            bomb.GetComponent<Bomb>().GetTimer().SetRateModifier(*(float*)&value);
+            return bomb;
         }
     }
 }
